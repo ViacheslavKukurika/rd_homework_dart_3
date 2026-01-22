bfdec451ca87983d9bbbabd1dbd6b4f5077a9fe8
onlytarg-clone <avdoninpav@gmail.com>
2026-01-22 09:01:09 +0200

    feat: finished with 1_math_operators_example.dart

--- lib/lesson_05(p)/1_math_operators_example.dart
+++ lib/lesson_05(p)/1_math_operators_example.dart
@@ -1,21 +1,41 @@
-// 📍 Математичні оператори
-void mathOperatorsExample() {
-  final a = 10;
-  final b = 3.4;
-  print('Додавання: ${a + b}');
-  print('Віднімання: ${a - b}');
-  print('Множення: ${a * b}');
-  print('Ділення: ${a / b}');
-  print('Цілочисельне ділення: ${a ~/ b}');
-  print('Залишок від ділення: ${a % b}');
-
-  print('');
-  print('Оруглення математичне: ${b.round()}');
-  print('Оруглення вгору: ${b.ceil()}');
-  print('Оруглення вниз: ${b.floor()}');
-  print('Зручний вивід в консоль: ${b.toStringAsFixed(2)}');
-}
-
 void main() {
-  mathOperatorsExample();
+  final a = 10;
+  final b = 3.14159265359;
+
+  final sum = a + b;
+  final difference = a - b;
+  final multiplication = a * b;
+  final division = a / b;
+
+  // print('-----------------------------------');
+  // print('sum  -  $sum');
+  // print('difference  -  $difference');
+  // print('multiplication  -  $multiplication');
+  // print('division  -  $division');
+  // print('-----------------------------------');
+
+  final simpleDivision = 12 / 5; // Звичайне ділення
+  final integerDivision = 12 ~/ 5; // Цілочисельне ділення
+  final remainder = 12 % 5; // Залишок від ділення
+  // 5 * 2 = 10    12 - 10 = 2
+
+  // print('-----------------------------------');
+  // print('Звичайне ділення  -  $simpleDivision');
+  // print('Цілочисельне ділення  -  $integerDivision');
+  // print('Залишок від ділення  -  $remainder');
+  // print('-----------------------------------');
+
+  // Найчастіше використовуються для округлення чисел
+  final someMoney = 25.5;
+  final rounded = b.round();
+  final ceiled = b.ceil();
+  final floored = b.floor();
+  final formatted = b.toStringAsFixed(2);
+
+  // print('-----------------------------------');
+  // print('rounded (Звичайне округлення) -  $rounded');
+  // print('ceiled (Округлення вгору) -  $ceiled');
+  // print('floored (Округлення вниз) -  $floored');
+  // print('formatted (Округлення до 2 знаків після коми) -  $formatted');
+  // print('-----------------------------------');
 }

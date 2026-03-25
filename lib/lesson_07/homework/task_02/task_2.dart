ea4302afd1ec695ef0bb406f99c17e875377ce34
Viacheslav <viacheslavkukurika@gmail.com>
2026-03-25 14:12:42 +0200

    Add homework task 2

--- lib/lesson_07/homework/task_02/task_2.dart
+++ lib/lesson_07/homework/task_02/task_2.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'package:dart_course/lesson_07/homework/task_02/names_deepseek.dart';
+import 'package:dart_course/lesson_07/homework/task_02/names_gpt.dart';
+
+void main() {
+
+  print('-----------------Спільні імена-----------------');
+
+  final commonSet = ukrainianNamesGPT.intersection(ukrainianNamesDeepseek);
+  print('К-ть спільних згенеров. імен(GPT + Deepseek): ${commonSet.length}');
+
+  print('--------------------GPT------------------------');
+
+  final uniqueGPT = ukrainianNamesGPT.difference(ukrainianNamesDeepseek);
+  print('Імена, які згенерував лише GPT: $uniqueGPT');
+
+  print('--------------------Deepseek--------------------');
+
+  final uniqueDeepseek = ukrainianNamesDeepseek.difference(ukrainianNamesGPT);
+   print('Імена, які згенерував лише Deepseek: $uniqueDeepseek');
+}

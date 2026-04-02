77ad50764a64306fb2a571ab27c6e0ffe9a5f652
onlytarg <onlytarg@gmail.com>
2026-04-02 13:12:52 +0300

    chore: Comment out example code in stream_from_iterable.dart for clarity and future reference

--- lib/lesson_10/streams/stream_from_iterable.dart
+++ lib/lesson_10/streams/stream_from_iterable.dart
@@ -5,3 +5,17 @@ void main() async {
     print('fromIterable: $value');
   }
 }
+
+// void main() async {
+//   print('🔹 Stream.fromIterable');
+
+//   const fullString = 'Привіт, мене звати Павло';
+//   final chrList = fullString.split('');
+
+//   final iterableStream = Stream.fromIterable(chrList);
+
+//   await for (final value in iterableStream) {
+//     stdout.write(value);
+//     await Future<void>.delayed(const Duration(milliseconds: 100));
+//   }
+// }
